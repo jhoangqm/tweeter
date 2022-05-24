715ff15a5aacc8e4b34e997569fcb350a592f42f
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-24 16:34:30 +0000

    Minor tweak to script counter using jQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,10 @@ let charLimit = 140;
 $(function () {
   // DOM LOADED SUCCESFULL
 
-  $('.new-tweet form textarea').on('input', function () {
+  // After the lecture I discovered you can assign the selector to a variable..
+  const $tweets = $('.new-tweet form textarea');
+
+  $tweets.on('input', function () {
     $(this).val((i, value) => {
       charLimit = 140 - value.length;
       const counter = $(this).siblings().find('.counter');
